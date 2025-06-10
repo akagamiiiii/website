da58e9a15be67522d1f44294f5b1091d33a263af
makoto <akagami8130@gmail.com>
2025-06-11 00:13:58 +0900

    登録画面表示~登録処理

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class EventController extends Controller
 {
@@ -14,20 +15,17 @@ class EventController extends Controller
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
+    //登録画面表示
     public function create()
     {
-        //
+        return view("events.create");
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+    //登録処理
     public function store(Request $request)
     {
-        //
+        Log::debug("イベント名： ". $request->get("title"));
+        return to_route("events.create");
     }
 
     /**
